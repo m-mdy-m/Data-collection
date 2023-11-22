f098745144e0667f6ea5a011137581e4cafcae81
mahdi <mahdimamashli1383@gmail.com>
2023-11-22 17:12:47 +0330

    finish and get price in value

--- script.js
+++ script.js
@@ -14,26 +14,27 @@ async function fetchHTML(url) {
 function getPrice(page) {
   let getPrice = page.querySelector(".coinPrice");
   let price = getPrice.innerHTML;
-  return price;
+  const priceNumber = Number(price.replace(/[^0-9.-]+/g,""));
+  return priceNumber;
 }
-function checkPrice(price) {
-  if (price > 40000) {
-    console.log("the price is over $40,000");
+function checkPrice(price, value) {
+  if (price > value) {
+    console.log(`the price is over $${value}`);
   } else {
-    console.log("the price is Not over $40,000");
+    console.log(`the price is Not over $${value}`);
   }
 }
 
 const cryptoObj = {
-  bitcoin: 40400,
-  ethereum: 20,
-  tether: 2,
+  "bitcoin": 40400,
+  "ethereum": 20,
+  "tether": 2,
 };
 for (const cryptoName in cryptoObj) {
   let URL = `https://arzdigital.com/coins/${cryptoName}`;
   fetchHTML(URL).then((page) => {
     const price = getPrice(page);
-    console.log(`the current price is ${price}`);
-    checkPrice(price);
+    console.log(`the current price of ${cryptoName} is $${price}`);
+    checkPrice(price,cryptoObj[cryptoName]);
   });
 }
